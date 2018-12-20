5d520046b007080a3854fb1ba170bf7d27c63d99
Sebastian Giraldo <jesegizu@hotmail.com>
2018-12-20 14:54:59 -0500

    Proyecto

---
+++ src/main/java/com/tns/metrotns/MetrotnsApplication.java
@@ -0,0 +1,14 @@
+package com.tns.metrotns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrotnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrotnsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tns/metrotns/MetrotnsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tns.metrotns;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MetrotnsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
